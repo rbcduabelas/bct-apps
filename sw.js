6175a2df7df23ed447b5ed763ca31afdaa305956
rbcduabelas <rbc.duabelas@gmail.com>
2026-05-23 21:00:34 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'bct-portal-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-mandiri-putih.png',
+  './logo-mandiri-biru.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
